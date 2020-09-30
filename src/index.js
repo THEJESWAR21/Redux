9e7bdba8485350470ff10131ea843b1ddf54a7e4
Thejeswar <64600548+THEJESWAR21@users.noreply.github.com>
2020-09-30 18:02:54 +0530

    Methoad 2 redux

--- src/index.js
+++ src/index.js
@@ -3,43 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
+import allReducer from "./reducers/Index";
+import { Provider } from "react-redux";
 
-//STORE -> GLOBALIZED STATE
-
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
   document.getElementById("root")
 );
 
